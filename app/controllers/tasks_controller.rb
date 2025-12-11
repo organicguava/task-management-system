1af3bd4dd34cec635116aa410fcbd2a580101f67
organicguava <cherryw.0301@gmail.com>
2025-12-11 18:14:33 +0800

    refactor task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,7 @@
 class TasksController < ApplicationController
+  # 將重複的程式碼抽出來，放到 private method 裡
+  before_action :set_task, only: %i[edit update destroy]
+
   def index
     # 使用chain，像接力棒一樣把查詢條件串起來
     @tasks = Task.all
@@ -16,8 +19,11 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      flash[:notice] = t("flash.tasks.create.notice")
-      redirect_to tasks_path # 成功後轉跳回列表頁
+      redirect_to tasks_path, notice: t("flash.tasks.create.notice")
+      # 將兩行合併為以上一行
+      # flash[:notice] = t("flash.tasks.create.notice")
+      # redirect_to tasks_path # 成功後轉跳回列表頁
+
     else
       # 驗證失敗時，重新渲染 new 頁面（停留在表單）
       flash.now[:alert] = t("flash.tasks.create.alert")
@@ -26,18 +32,13 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    # 1. 先找到舊資料 (跟 edit action 一樣)
-    @task = Task.find(params[:id])
-
-    # 2. 嘗試更新 (使用 Strong Parameters)
+    # 嘗試更新 (使用 Strong Parameters)
     if @task.update(task_params)
       # 成功：設定 Flash 訊息並轉跳回列表
-      flash[:notice] = t("flash.tasks.update.notice")
-      redirect_to tasks_path
+      redirect_to tasks_path, notice: t("flash.tasks.update.notice")
     else
       # 驗證失敗時停留在 edit 頁面並顯示錯誤訊息
       flash.now[:alert] = t("flash.tasks.update.alert")
@@ -46,15 +47,20 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id]) # 如果找不到 id，Rails 會自動跳 404，這行以下都不會執行
+    # 如果找不到 id，Rails 會自動跳 404，這行以下都不會執行
     @task.destroy
-    flash[:notice] = t("flash.tasks.destroy.notice")
-    redirect_to tasks_path, status: :see_other # 必須回傳 HTTP 303 (See Other) 狀態碼，否則 Turbo 有時候會報錯。
+    redirect_to tasks_path, status: :see_other, notice: t("flash.tasks.destroy.notice")
+    # 必須回傳 HTTP 303 (See Other) 狀態碼，否則 Turbo 有時候會報錯。
   end
 
   private # 為了安全性，必須使用 Strong Parameters
 
+  # edit update destroy 都有此動作，提取出來成為共用方法
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:title, :content, :end_time)
+    params.require(:task).permit(:title, :content, :end_time, :status)
   end
 end
