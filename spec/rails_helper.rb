82ec2ef6353448cc0c39b322775ac149165d09a8
organicguava <cherryw.0301@gmail.com>
2025-12-18 16:33:03 +0800

    fix/browser crash error in CI environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
+# 確保 Capybara 函式庫被載入，這是 Feature Spec 運作的基礎
+require 'capybara/rspec'
+require 'capybara/rails'
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
@@ -12,74 +16,75 @@ end
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-# Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
-# that will avoid rails generators crashing because migrations haven't been run yet
-# return unless Rails.env.test?
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
-
-# Ensures that the test database schema matches the current schema file.
-# If there are pending migrations it will invoke `db:test:prepare` to
-# recreate the test database by loading the schema.
-# If you are not using ActiveRecord, you can remove these lines.
+# Checks for pending migrations and applies them before tests are run.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
   # 讓 RSpec 認識 FactoryBot 的語法
   config.include FactoryBot::Syntax::Methods
+
   # 讓 feature spec 可以使用 t() helper
   config.include ActionView::Helpers::TranslationHelper
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
 
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
+  # 必須設為 false，因為我們要手動控制 DatabaseCleaner
+  # 這是避免 CI 資料殘留的關鍵設定
   config.use_transactional_fixtures = false
 
-  # 設定 DatabaseCleaner (請將以下整段貼入 config 區塊內)
+  # --- DatabaseCleaner 設定 (符合專案守則) ---
 
-  # 【設定 A】：在整個 RSpec 跑起來前，先做一次徹底的「截斷 (Truncation)」
-  # 解決 CI 髒資料 (之前測試殘留的) 的關鍵！它會把資料表清空重置。
+  # 在整個 Suite 開始前，徹底清空資料庫 (Truncation)
+  # 這能解決 CI 環境中可能殘留的髒資料
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  # 【設定 B】：每一個 individual example (it) 執行前後，使用交易 (Transaction) 的方式來確保資料庫乾淨
+  # 每一個測試執行前後，自動切換策略
   config.around(:each) do |example|
-    # 如果這個測試有標記 js: true (代表是瀏覽器驅動)，就使用 :truncation (慢但穩)
-    # 否則 (一般單元測試)，使用 :transaction (快且安全)
+    # 如果是 js: true (瀏覽器測試) 用 truncation，否則用 transaction
     DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
 
-    # cleaning 方法會自動執行 start (開始前) 和 clean (結束後)
     DatabaseCleaner.cleaning do
       example.run
     end
   end
+  # --- DatabaseCleaner 設定結束 ---
 
-  # Filter lines from Rails gems in backtraces.
+  # --- Capybara Driver 切換設定 ---
+  config.before(:each, type: :feature, js: true) do
+    if ENV['CI']
+      # CI 環境：使用無頭模式 (Headless) 以避免崩潰
+      Capybara.current_driver = :selenium_chrome_headless_in_ci
+    else
+      # 本地環境：使用一般 Chrome (可視化) 以便除錯
+      Capybara.current_driver = :selenium_chrome
+    end
+  end
+
+  # 確保此設定在 configure 區塊內
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+end
+
+# --- 註冊 CI 專用的 Headless Chrome Driver ---
+# 放在 RSpec.configure 區塊外面是正確的
+Capybara.register_driver :selenium_chrome_headless_in_ci do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+
+  # CI 環境必備參數，防止崩潰
+  options.add_argument("--headless")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--window-size=1400,1400")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
